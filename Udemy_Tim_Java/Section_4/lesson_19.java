e3b31d5900e04f4423efaa498917ac19c9ab7137
azharahamed <azharahamed@users.noreply.github.com>
2018-06-16 20:53:59 -0400

    completed the lessen 19 java

---
+++ Udemy_Tim_Java/Section_4/lesson_19.java
@@ -0,0 +1,12 @@
+public class lesson_19{
+  public static void main(String args[]) {
+    char myChar = '\u00AE';
+    System.out.println("Symbol " + myChar);
+
+    boolean myBoolean = false;
+    boolean isMale = true;
+
+
+    
+  }
+}
