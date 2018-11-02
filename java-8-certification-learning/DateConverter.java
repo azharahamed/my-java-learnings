5b40babfaac2c1802fc6b57f13a924e9f9bc8fb4
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-11-01 21:19:50 -0400

    Worked on the DateConvertor.java

--- java-8-certification-learning/DateConverter.java
+++ java-8-certification-learning/DateConverter.java
@@ -2,27 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 
 class DateConvertor {
     public static void main(String []args){
-        List<String> dates = new ArrayList<>();
-        dates.add("1/1/11");
-        dates.add("01/01/11");
-        dates.add("01/01/2011");
-        dates.add("01/1/2011");
-        dates.add("1/11/2011");
-        dates.add("21/09/11");
-        dates.add("11/1/11");
-        
-        String regex = "^(?:[0-9]{2})?[0-9]{2}/([0-1][0-9])|([0-9])/[0-3]?[0-9]$";
-        
-        Pattern pattern = Pattern.compile(regex);
-        
-        for(String date : dates)
-        {
-            Matcher matcher = pattern.matcher(date);
-            System.out.println(date +" : "+ matcher.matches());
+        try{
+
+       
+        String str = "1/1/99";
+        if (!str.matches("^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$") && !str.matches("^[0-3]?[0-9]-[0-3]?[0-9]-(?:[0-9]{2})?[0-9]{2}$")){
+            System.out.println("Didn't match");
         }
+        if (str.matches("^[0-9].+[0-3]?[0-9]$")){
+            System.out.println("Matches Mine");
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date dt = sdf.parse(str);
+        int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(dt));
+		int month = Integer.parseInt(new SimpleDateFormat("MM").format(dt));
+		int day = Integer.parseInt(new SimpleDateFormat("dd").format(dt));
+        System.out.println("The date is "+ dt + " Year: " + year + " Month "+ month +" Day "+ day);
+    } catch(Exception e){
+        System.out.println("Exception");
+    }
     }
     
 }
