2a19eb75d6fcf3bb1f8707788db2fdece24aa1e0
azharahamed <azharahamed@users.noreply.github.com>
2018-12-11 22:58:49 -0500

    Excersises

--- java-8-certification-learning/Q3.java
+++ java-8-certification-learning/Q3.java
@@ -1,8 +1,9 @@
 class Q3 {
   public static void main(String[] args) {
-    int ints[][] = new int[3][];
-    ints[1] = new int[]{1,2,3};
-    ints[2] = new int[]{4,5};
-    System.out.print(ints[1][1]);
+    int []ints = new int[10];
+    int len = ints.length;
+    for(int i:ints){
+      System.out.print(i);
+    }
   }
 }
