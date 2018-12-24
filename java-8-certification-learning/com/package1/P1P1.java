0ed407e6af0dafc5c2b4e197a3618e12b2caed67
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-12-24 11:22:42 -0500

    packages

---
+++ java-8-certification-learning/com/package1/P1P1.java
@@ -0,0 +1,11 @@
+package com.package1;
+
+public class P1P1 {
+    protected String name;
+    protected String getName(){
+        return this.name;
+    }
+    protected void setName(String name){
+        this.name = name;
+    }
+}
