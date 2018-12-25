d7bdb51c06cacc2074f2f4e965d8a780714a667a
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-12-24 19:39:22 -0500

    Class Initializaion Order

---
+++ java-8-certification-learning/InitializationOrderSimple.java
@@ -0,0 +1,15 @@
+public class InitializationOrderSimple {
+    private String name = "Torchie";
+    {System.out.println(name);}
+    private static int COUNT=0;
+    static {System.out.println(COUNT);}
+    static {COUNT+=10;System.out.println(COUNT);}
+    public InitializationOrderSimple(){
+        System.out.println("Constructor");
+    }
+
+    public static void main(String args[]){
+        System.out.println("ready to contruct");
+        new InitializationOrderSimple (); 
+    }
+}
