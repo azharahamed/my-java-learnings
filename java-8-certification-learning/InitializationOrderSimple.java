189415e3a01d035998280abb460b3df39eb3833f
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-12-24 20:43:14 -0500

    Initialisation order

--- java-8-certification-learning/InitializationOrderSimple.java
+++ java-8-certification-learning/InitializationOrderSimple.java
@@ -10,6 +10,5 @@ public class InitializationOrderSimple {
 
     public static void main(String args[]){
         System.out.println("ready to contruct");
-        new InitializationOrderSimple (); 
     }
 }
