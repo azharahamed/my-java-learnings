71758240512b6a03d538a09570a3ba81e5e44180
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-12-24 19:12:52 -0500

    Overload

--- java-8-certification-learning/OverloadOrder.java
+++ java-8-certification-learning/OverloadOrder.java
@@ -1,3 +1,4 @@
+
 public class OverloadOrder {
     public void fly(Double d){
         System.out.println("Double ");
@@ -10,12 +11,19 @@ public class OverloadOrder {
     public void fly(Long l){
         System.out.println("Long ");
     }
-    public void fly(Integer i){
-        System.out.println("Int ");
+
+    public void fly(Object o){
+        System.out.println("Object ");
     }
+
+    // public void fly(Integer i){
+    //     System.out.println("Int ");
+    // }
 
     public static void main(String[] args){
         OverloadOrder o = new OverloadOrder();
         o.fly(10);
     }
 }
+
+class Second {}
