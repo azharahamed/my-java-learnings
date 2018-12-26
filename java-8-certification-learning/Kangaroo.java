d8a6e331fb8e69b094a25107db64ac677ab438bb
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-12-26 14:02:32 -0500

    Overriding Vs Hiding Methods

--- java-8-certification-learning/Kangaroo.java
+++ java-8-certification-learning/Kangaroo.java
@@ -1,18 +1,27 @@
 class Marsupial {
-    public boolean isBiped(){
+    public int a = 10;
+    static public boolean isBiped(){
         return false;
     }
     public void getMarsupialDescription() {
-        System.out.println("Marsupial walks on two legs: "+isBiped());
+        System.out.println("Marsupial walks on two legs: "+isBiped()+" > "+a);
     }
 }
 
 public class Kangaroo extends Marsupial {
-    public boolean isBiped() {
+    public int a = 20;
+    static public boolean isBiped() {
         return true;
     }
     public void getKangarooDescription(){
-        System.out.println("Kangaroo hops on two legs: "+isBiped());
+        System.out.println("Kangaroo hops on two legs: "+isBiped() + "| " + this.a);
+        System.out.println ("Super");
+        super.getMarsupialDescription();
+        System.out.println("This");
+        this.getMarsupialDescription();
+    }
+    public void getMarsupialDescription() {
+        System.out.println("Marsupial walks on two legs hjkh: "+isBiped()+" > "+a);
     }
 
     public static void main(String[] args){
