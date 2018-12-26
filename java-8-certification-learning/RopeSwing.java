6d56215ee9b1224b6910d97e9a2f18d5a7b6b096
azharahamed <12764179+azharahamed@users.noreply.github.com>
2018-12-25 21:46:25 -0500

    Constructor call

---
+++ java-8-certification-learning/RopeSwing.java
@@ -0,0 +1,19 @@
+public class RopeSwing{
+    private static final String leftRope;
+    private static final String rightRope;
+    private static final String bench;
+    private static final String name = "name";
+    static {
+        leftRope = "left";
+        rightRope = bench = "right";
+        
+        
+    }
+    static {
+       // name = "name";
+        
+    }
+    public static void main(String[] args){
+        // bench = "bench";
+    }
+}
