f61935236a73f86ee634fc9ca5f3a88255b9a95d
azhar <12764179+azharahamed@users.noreply.github.com>
2019-03-09 09:35:05 -0500

    Access Modifiers and Packages

---
+++ java-8-ocp-learning/packages/P2A.java
@@ -0,0 +1,12 @@
+package p2;
+import p1.*;
+
+public class P2A{
+    public static void main(String[] args) {
+        P1A p1a = new P1A();
+        System.out.println(p1a.publicV);
+       //System.out.println(p1a.protectedV);
+       //System.out.println(p1a.defaultV);
+        //System.out.println(p1a.privateV);
+    }
+}
