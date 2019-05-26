da98a7e047b340d69918d7fbb95244dafd442c71
azhar <12764179+azharahamed@users.noreply.github.com>
2019-05-26 11:02:21 -0400

    Chapter 1 Review

--- java-8-ocp-learning/Chapter1/FourLegged.java
+++ java-8-ocp-learning/Chapter1/FourLegged.java
@@ -1,13 +1,26 @@
 public class FourLegged {
     String walk = "walk, ";
+
+    public static String test(){
+        return " -- Walk --";
+    }
     static class BabyRhino extends FourLegged{
         String walk = "toddle , ";
 
+        public static String test(){
+            return " -- Toddle --";
+        }
+
     }
 
-    public static void main(String [] args){
+    public void callOuter(){
         FourLegged f = new BabyRhino();
         BabyRhino b = new BabyRhino();
         System.out.println(f.walk + b.walk);
+        System.out.println(f.test() + b.test());
+    }
+
+    public static void main(String [] args){
+        new FourLegged().callOuter();
     }
 }
