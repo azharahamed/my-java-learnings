24551dd7aaf88d144b5e8c9a2d49aa626af299eb
azhar <12764179+azharahamed@users.noreply.github.com>
2019-07-02 00:46:26 -0400

    Bounded Wildcards

---
+++ java-8-ocp-learning/Chapter3/boundedExtend.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+class Sparrow extends Bird{
+    public static void printName(){
+        System.out.println("Sparrow");
+    }
+}
+class Bird {
+    public static void printName(){
+        System.out.println("Bird");
+    }
+}
+public class boundedExtend {
+    public static void main(String [] args){
+        List<? super Bird> birds = new ArrayList<>();
+        Sparrow sparrow = new Sparrow();
+        Bird bird = new Bird();
+        birds.add(sparrow);
+        birds.add(bird);
+    }
+}
