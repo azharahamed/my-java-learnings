95415c2f432356da0fec359e40530e570cbad9bb
Azhar Ahamed <azharahamed@azhars-mbp.home>
2020-08-23 00:09:34 -0400

    Learning Primitive

--- Udemy_Tim_Java/IntTest.java
+++ Udemy_Tim_Java/IntTest.java
@@ -3,10 +3,14 @@ public class IntTest {
         int myMinValue = Integer.MIN_VALUE;
         int myMaxValue = Integer.MAX_VALUE;
 
+        long testLong = 100L;
+
         System.out.println("Integer Minimum Value " + myMinValue);
         System.out.println("Integer Maximum Value " + myMaxValue);
 
         System.out.println("Busted Minimum Value " + (myMinValue - 1));
         System.out.println("Busted Maximum Value " + (myMaxValue + 1));
+
+        System.out.println("Long value = " + testLong);
     }
 }
